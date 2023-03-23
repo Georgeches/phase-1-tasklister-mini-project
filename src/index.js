a0ac8deb25a79b5c0fb9e53952974257d76de705
Georgeches <georgechesire83@gmail.com>
2023-03-23 12:14:27 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  let toDoList = document.querySelector("#tasks")
+  form.addEventListener('submit', addToDo)
+
+  function addToDo(e){
+    e.preventDefault()
+    let newToDo = document.createElement('li')
+    let deleteToDo = document.createElement('button')
+    let taskValue = document.getElementById("new_task").value
+    deleteToDo.textContent = 'X'
+    deleteToDo.addEventListener('click', deleteTask)
+    newToDo.textContent = `${taskValue}      `
+    newToDo.appendChild(deleteToDo)
+    toDoList.appendChild(newToDo)
+    form.reset()
+  }
+
+  function deleteTask(e){
+    e.target.parentNode.remove()
+  }
 });
